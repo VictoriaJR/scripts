f7a4bd6ae349d5ea257eaef911de8ea1210b3cdb
VictoriaJR <77169415+VictoriaJR@users.noreply.github.com>
2021-02-07 13:31:00 -0500

    Add files via upload

---
+++ assembly_pipeline_unpaired.jl
@@ -0,0 +1,317 @@
+"""
+    transcriptome_assembly_unpaired(dir_path, organism, lineage_dataset, prey, steps)
+
+Execute the transcriptome assembly pipeline consisting of the steps: "fastqc", "cutadapt", "rnaspades", "busco", "blastn_megablast", "diamond_blastx", "bowtie2", "blobtools", "contamination_removal", "prey_removal", "transdecoder".
+Inputs:
+- `dir_path` = dir_path of the two raw sequence reads files
+- `organism` = name of the organism under study. This will be used as a prefix throughout
+- `lineage_dataset` = e.g. /PATH/TO/DATABASE/eukaryota_odb10, /PATH/TO/DATABASE/alveolata_odb10
+- `contaminations` = e.g. ["Chordata", "Bacteria",  "Arthropoda"]
+- `prey` = name of the prey, e.g. "Procryptobia" for Colp34, "Spumella" for Psammosa pacifica
+- `steps` = steps of the pipeline to run. Default to all.
+"""
+function transcriptome_assembly_unpaired(dir_path::AbstractString, organism::AbstractString, lineage_dataset::AbstractString, contaminations, prey::AbstractString, steps=String[])
+    if dir_path[end] != "/"
+        dir_path *= "/"
+    end
+    isdir(dir_path) || return throw(ArgumentError(string("Input path ", dir_path, " is not a directory")))
+
+    check_fastqc = false
+    check_cutadapt = false
+    check_rnaspades = false
+    check_busco = false
+    check_blastn_megablast = false
+    check_diamond_blastx = false
+    check_bowtie2 = false
+    check_blobtools = false
+    check_contamination_removal = false
+    check_prey_removal = false
+    check_transdecoder = false
+
+    if isempty(steps)
+        check_fastqc = true
+        check_cutadapt = true
+        check_rnaspades = true
+        check_busco = true
+        check_blastn_megablast = true
+        check_diamond_blastx = true
+        check_bowtie2 = true
+        check_blobtools = true
+        check_contamination_removal = true
+        check_prey_removal = true
+        check_transdecoder = true
+    else
+        for step in steps
+            if step == "fastqc"
+                check_fastqc = true
+            elseif step == "cutadapt"
+                check_cutadapt = true
+            elseif step == "rnaspades"
+                check_rnaspades = true
+            elseif step == "busco"
+                check_busco = true
+            elseif step == "blastn_megablast"
+                check_blastn_megablast = true
+            elseif step == "diamond_blastx"
+                check_diamond_blastx = true
+            elseif step == "bowtie2"
+                check_bowtie2 = true
+            elseif step == "blobtools"
+                check_blobtools = true
+            elseif step == "contamination_removal"
+                check_contamination_removal = true
+            elseif step == "prey_removal"
+                if prey == "Procryptobia"
+                    prey_db = "/Data/victoria/transcriptomes/Procryptobia/Procryptobia_both.fa.DB"
+                elseif prey == "Spumella"
+                    prey_db = "/Data/victoria/transcriptomes/Spumella_elongata_MMETSP1098/Spumella_elongata_CCAP955_1_MMETSP1098_cds.fa.db"
+                else
+                    return throw(ArgumentError(string("Prey ", prey, " is not valid")))
+                end
+                check_prey_removal = true
+            elseif step == "transdecoder"
+                check_transdecoder = true
+            else
+                return throw(ArgumentError(string("Step ", step, " is not valid")))
+            end
+        end
+    end
+
+
+
+
+
+    ## 1. TRIM ADAPTERS/PRIMERS FROM RAW READS
+
+    # use FASTQC for quality analysis
+
+    fastqc_dir = dir_path * "fastqc_raw_reads/"
+    raw_seq_reads = dir_path * organism * ".fastq"
+    if check_fastqc
+        if !isdir(fastqc_dir)
+            mkdir(fastqc_dir)
+        end
+        run(`fastqc -o $fastqc_dir $raw_seq_reads`)
+    end
+
+    # use CUTADAPT to remove adapters from paired-end reads
+
+    cutadapt_dir = dir_path * "cutadapt/"
+    cutadapt_output_file = cutadapt_dir * organism * ".cutadapt.fastq"
+    if check_cutadapt
+        if !isdir(cutadapt_dir)
+            mkdir(cutadapt_dir)
+        end
+        run(`cutadapt
+            -a CCCATGTACTCTGCGTTGATACCACTGCTT
+            -o $cutadapt_output_file
+            $raw_seq_reads`)
+    end
+
+
+
+
+
+    ## 2. TRANSCRIPTOME ASSEMBLY
+
+    # use SPADES to assemble
+
+    rnaspades_dir = dir_path * "../" * organism * "_rnaspades/"
+    if check_rnaspades
+        if !isdir(rnaspades_dir)
+            mkdir(rnaspades_dir)
+        end
+        run(`/Data/victoria/software/SPAdes-3.15.0-Linux/bin/spades.py
+            --rna
+            --s1 $cutadapt_output_file
+            --threads 24
+            --memory 100
+            -o $rnaspades_dir`)
+    end
+    transcripts_file = rnaspades_dir * "transcripts.fasta"
+
+    # use BUSCO to estimate transcriptome coverage
+
+    if check_busco
+        path_ = pwd()
+        cd("/Data/victoria/db/")
+        run(`busco
+            --in $transcripts_file
+            --out $(organism * "_transcripts.fasta_BUSCO")
+            --out_path $rnaspades_dir
+            --lineage_dataset $lineage_dataset
+            --mode transcriptome`)
+        cd(path_)
+    end
+
+
+
+
+
+    ## 3. LOOK FOR CONTAMINATION
+
+    contamination_dir = rnaspades_dir * organism * "_contamination_removal/"
+    if !isdir(contamination_dir)
+        mkdir(contamination_dir)
+    end
+
+    # use BLASTN to perform a megablast with nt database
+
+    blastn_output_file = contamination_dir * "transcripts_vs_nt.blastn"
+    if check_blastn_megablast
+        run(`blastn
+            -task megablast
+            -query $transcripts_file
+            -db /Data/databases/NCBI_NT/nt
+            -outfmt '6 qseqid staxids bitscore std ssciorganisms sskingdoms stitle'
+            -culling_limit 5
+            -num_threads 24
+            -evalue 1e-25
+            -max_target_seqs 5
+            -out $blastn_output_file`)
+    end
+
+    # use DIAMOND BLASTX
+
+    blastx_output_file = contamination_dir * "transcripts.fasta.vs.uniprot_ref.mts1.1e25.out"
+    if check_diamond_blastx
+        run(`/home/glax/miniconda3/bin/diamond blastx
+            --query $transcripts_file
+            --max-target-seqs 1
+            --sensitive
+            --threads 24
+            --db /Data/databases/uniprot_ref_v2020.06_diamond/uniprot_ref_proteomes.diamond.dmnd
+            --evalue 1e-25
+            --outfmt 6
+            --out $blastx_output_file`)
+    end
+
+    # use BOWTIE2
+
+    bowtie2_prefix = organism * "_transcripts.fasta"
+    bowtie2_output_file = contamination_dir * organism * "_rnaspades.sam"
+    if check_bowtie2
+        path_ = pwd()
+        cd(rnaspades_dir)
+        run(`bowtie2-build $transcripts_file $bowtie2_prefix`)
+        run(`bowtie2
+            -x $bowtie2_prefix
+            -U $cutadapt_output_file
+            -S $bowtie2_output_file
+            -p 24`)
+        cd(path_)
+    end
+
+    # use BLOBTOOLS
+
+    blobtools_taxify_output_file = contamination_dir * "transcripts.fasta.vs.uniprot_ref.mts1.1e25.taxified.out"
+    blobtools_prefix = organism * "_rnaspades"
+    blobtools_map2cov_output_file = contamination_dir * blobtools_prefix * ".sam.cov"
+    blobtools_create_output_file = contamination_dir * blobtools_prefix * ".blobDB.json"
+    if check_blobtools
+        path_ = pwd()
+        cd(contamination_dir)
+        run(`/opt/blobtools/blobtools taxify
+            -f $blastx_output_file
+            -m /Data/databases/uniprot_ref_v2020.06_diamond/uniprot_ref_proteomes.taxids
+            -s 0
+            -t 2`)
+        run(`/opt/blobtools/blobtools map2cov -i $transcripts_file -s $bowtie2_output_file`)
+        run(`/opt/blobtools/blobtools create
+            -i $transcripts_file
+            -t $blastn_output_file
+            -t $blobtools_taxify_output_file
+            -c $blobtools_map2cov_output_file
+            -o $blobtools_prefix`)
+        run(`/opt/blobtools/blobtools blobplot
+            --infile $blobtools_create_output_file
+            --rank family`)
+        run(`/opt/blobtools/blobtools blobplot
+            --infile $blobtools_create_output_file
+            --rank phylum`)
+        run(`/opt/blobtools/blobtools blobplot
+            --infile $blobtools_create_output_file
+            --rank superkingdom`)
+        run(`/opt/blobtools/blobtools view
+            --input $blobtools_create_output_file
+            --out taxonomy
+            --rank all
+            --taxrule bestsum`)
+        cd(path_)
+    end
+    blobtools_view_output_file = contamination_dir * "taxonomy." * blobtools_prefix * ".blobDB.bestsum.table.txt"
+
+    # clean transcripts
+
+    clean_transcripts_file = transcripts_file
+
+    if check_contamination_removal
+        for name in contaminations
+            contigs_list = contamination_dir * "no_" * name * "_contigs.list"
+            run(pipeline(`grep -Ev $name $blobtools_view_output_file`, `cut -f 1`, contigs_list))
+            clean_transcripts_file = lookup_match(clean_transcripts_file, name, contigs_list)
+        end
+    end
+
+    if check_prey_removal
+        removed_contaminations = ""
+        for name in contaminations
+            removed_contaminations *= "no_" * name * "_" # e.g. "no_Chordata_no_Bacteria_no_Arthropoda_"
+        end
+        prey_blastn_output_file = contamination_dir * organism * "_" * removed_contaminations * "vs_" * prey * ".blastnout"
+        run(`blastn
+            -task megablast
+            -query $clean_transcripts_file
+            -db $prey_db
+            -outfmt 6
+            -num_threads 24
+            -evalue 1e-25
+            -max_target_seqs 1
+            -out $prey_blastn_output_file`)
+        prey_contigs_list = contamination_dir * prey * "_contigs.list"
+        run(pipeline(`cut -f 1`, prey_blastn_output_file, prey_contigs_list))
+        clean_transcripts_file = lookup_mistmatch(clean_transcripts_file, prey, prey_contigs_list)
+    end
+
+
+
+
+
+    ## 4. TRANSLATE TO PEPTIDES
+
+    if check_transdecoder
+        path_ = pwd()
+        cd(rnaspades_dir)
+        # Identify the ORFs in the cleaned fasta assembly using ###TransDecoder:
+
+        run(`perl5.18.2 /opt/TransDecoder-TransDecoder-v5.3.0/TransDecoder.LongOrfs -t $clean_transcripts_file`)
+        # Outputs peptides in a new TransDecoder directory as a file named "long_orfs.pep".
+
+        # blast the ORFs against the Swissprot database to find ORFs that have possible matches to known proteins:
+        database = "/Data/databases/uniprot_sprot_2020.12.02/uniprot_sprot.fasta"
+        for f in readdir("/Data/databases/uniprot_sprot_2020.12.02/"; join = false)
+            if startswith(f, "uniprot_sprot_")
+                database *= f
+                break
+            end
+        end
+        blastp_output = rnaspades_dir * organism * "_longest_orfs.blastout"
+        run(`blastp
+            -query $(clean_transcripts_file * ".transdecoder_dir/longest_orfs.pep")
+            -db $database
+            -evalue 1e-5
+            -max_target_seqs 1
+            -outfmt 6
+            -out $blastp_output
+            -num_threads 24`)
+
+        # Run the final ORF annotation using TransDecoder and tell it to include the ORFs that were identified in the Blast search as being things that matched to lessen the chance that they are lost in TransDecoder:
+
+        run(`perl5.18.2 /opt/TransDecoder-TransDecoder-v5.3.0/TransDecoder.Predict
+            -t $clean_transcripts_file
+            --retain_blastp_hits $blastp_output`)
+        cd(path_)
+
+        dir_rename_headers(rnaspades_dir, ".transdecoder.pep",  organism)
+    end
+end
