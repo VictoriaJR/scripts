13bacc5871cc603366be0c07f8e544ea78fdcf42
VictoriaJR <77169415+VictoriaJR@users.noreply.github.com>
2021-02-17 10:35:16 -0800

    Update assembly_pipeline_db.jl
    ff

--- assembly_pipeline_db.jl
+++ assembly_pipeline_db.jl
@@ -10,7 +10,7 @@ Inputs:
 - `prey` = name of the prey, e.g. "Procryptobia" for Colp34, "Spumella" for Psammosa pacifica
 - `steps` = steps of the pipeline to run. Default to all.
 """
-function transcriptome_assembly(dir_path::AbstractString, organism::AbstractString, lineage_dataset::AbstractString, contaminations, prey::AbstractString, steps=String[])
+function transcriptome_assembly_db(dir_path::AbstractString, organism::AbstractString, lineage_dataset::AbstractString, contaminations, prey::AbstractString, steps=String[])
     if dir_path[end] != "/"
         dir_path *= "/"
     end
