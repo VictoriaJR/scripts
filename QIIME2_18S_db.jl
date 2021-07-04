46579d9ab6b3d8f13a698962ee47d2149376cc1e
VictoriaJR <77169415+VictoriaJR@users.noreply.github.com>
2021-07-04 15:24:20 -0700

    Add files via upload

--- QIIME2_18S_db.jl
+++ QIIME2_18S_db.jl
@@ -81,7 +81,7 @@ function QIIME2_18S_db(dir_path::AbstractString, bio_project::AbstractString, se
         if !isdir(fastq_dir)
             mkdir(fastq_dir)
         end
-        run(`cd fastq_dir`)
+        run(`cd $fastq_dir`)
         if sequencing_type == "single_end"
             for line in eachline(sra_acc_file)
                 run(`/opt/sratoolkit.2.10.9-ubuntu64/bin/fastq-dump $line --gzip`)
